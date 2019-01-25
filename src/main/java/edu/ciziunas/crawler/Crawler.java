b9b742cbc72f7e908659261417e97365beec7f5e
mariusciziunas <mr.marius.c@gmail.com>
2019-01-25 07:35:24 +0000

    algorithm in order to build a tree implemented

--- src/main/java/edu/ciziunas/crawler/Crawler.java
+++ src/main/java/edu/ciziunas/crawler/Crawler.java
@@ -1,6 +1,11 @@
 package edu.ciziunas.crawler;
 
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Crawler {
 
@@ -10,11 +15,32 @@ public class Crawler {
     }
 
     private Node browse(String URL) {
-
-
+        List<String> childrenURLs = getChildrenUrls(URL);
+        Node result = new Node(URL);
+        if (childrenURLs != null && !childrenURLs.isEmpty()) {
+            for (String child : childrenURLs) {
+                result.addChild(browse(child));
+            }
+        }
+        return result;
     }
 
-    private List<String> getUrls() {
+    protected List<String> getChildrenUrls(String root) {
+//        Pattern urlPattern = Pattern.compile(".*<a.*<\\/a>");
 
+        try (InputStream is = new URL(root).openConnection().getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                Stream<String> stream = reader.lines()) {
+//".*<a.*<\/a>"
+//            ".*<a.*href=".*".*<\/a>"
+            stream.filter(line ->  Pattern.matches(".*<a.*<\\/a>", line)).forEach(System.out::println);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
--- src/main/java/edu/ciziunas/crawler/Node.java
+++ src/main/java/edu/ciziunas/crawler/Node.java
@@ -1,11 +1,22 @@
 package edu.ciziunas.crawler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
 
     private String url;
-    private List<Node> children;
+    private List<Node> children = new ArrayList<Node>();
 
+    public Node(String url) {
+        this.url = url;
+    }
 
+    public List<Node> getChildren() {
+        return children;
+    }
+
+    public void addChild(Node node) {
+        children.add(node);
+    }
 }
